f0769d7cbdbfd1d5556860aecb87c6dfd9ee0a46
parsee <babakovi679@gmail.com>
2025-08-17 16:12:23 +0000

    done

--- week_5_task/TEST_RUN.sh
+++ week_5_task/TEST_RUN.sh
@@ -17,5 +17,6 @@ cd ../..
 # ./build/bin/opt -passes=dot-cfg -cfg-func-name="req1" input.ll -cfg-dot-filename-prefix=cfg_output -o /dev/null
 clang -S -O0 -emit-llvm ./llvm-my-pass/tests/input.c -o input.ll
 opt -load-pass-plugin ./llvm-my-pass/build/libMyPass.so -passes=MyPass -disable-output input.ll
+# my ignore -cfg-func-name because of older version of opt
 opt -passes=dot-cfg -cfg-func-name="req1" input.ll -cfg-dot-filename-prefix=cfg_output -o /dev/null
 dot -Tpng cfg_output.req1.dot -o cfg_output.req1.png
